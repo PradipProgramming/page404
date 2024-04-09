a1cc6d3b3c79eed832cb3c2fb43c24f96549a4e0
Pradip Malik <93438482+PradipProgramming@users.noreply.github.com>
2024-04-09 11:30:02 +0530

    page404
    Page Not Found page create

---
+++ page404.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ASPG Food Restaurent About Page</title>
+
+  <!-- css file link  -->
+  <link rel="stylesheet" href="style5.css">
+
+  
+
+ <!-- bootstrap file link  -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" integrity="sha384-ejwKkLla8gPP8t2u0eQyL0Q/4ItcnyveF505U0NIobD/SMsNyXrLti6CWaD0L52l" crossorigin="anonymous">
+
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://kit.fontawesome.com/6f9a045125.js" crossorigin="anonymous"></script>
+
+</head>
+<body class="op">
+  <div class="wrapper">
+    <h1><b>Oops! Page not found.</b></h1>
+    <div>
+      <img src="images/capture404.jpg">
+    </div>
+    <h4>We can't find the page you're looking for.</h4>
+    <input type="button" value="GO Back!" onclick="history.back()">
+    <!--<input type="button" value="GO Back!" onclick="history.go(-1)">-->
+  </div>
+</body>
+</html>
